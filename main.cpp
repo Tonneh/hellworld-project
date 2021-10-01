1cc173d4d1d2ded750f515c1c1da95399805a753
Tonneh <tonyle.1537@gmail.com>
2021-10-01 14:00:36 -0700

    moving

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "utilities.cpp"
+
+using namespace std;
+
+void helper();
+
+int main()
+{
+    cout << "hello world" << endl;
+    helper();
+}
---
+++ utilities.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using namespace std;
+
+void helper()
+{
+    cout << "good" << endl;
+}
